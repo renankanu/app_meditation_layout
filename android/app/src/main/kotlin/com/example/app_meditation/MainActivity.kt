ea74c62b56b19755e5bb965f7fbbea6906ef711a
renankanu <renankanu@gmail.com>
2020-08-30 20:20:00 -0300

    Inirial Commit

---
+++ android/app/src/main/kotlin/com/example/app_meditation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_meditation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:app_meditation/app/screen/intro/intro_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Intro(),
+    );
+  }
+}
