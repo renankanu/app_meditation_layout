3a74ffd7f92ed32749be352e9c8c37f9080e14d3
renankanu <renankanu@gmail.com>
2020-08-30 21:04:24 -0300

    Feature - Create splash screen

--- lib/app/screen/intro/intro_screen.dart
+++ lib/app/screen/intro/intro_screen.dart
@@ -1,9 +1,32 @@
-import 'package:flutter/cupertino.dart';
+import 'package:app_meditation/app/animations/scale_animation.dart';
+import 'package:app_meditation/app/screen/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class Intro extends StatelessWidget {
+class Intro extends HookWidget {
+  const Intro({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final animationController = useAnimationController();
+
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/lottie/meditation.json',
+          onLoaded: (composition) {
+            animationController.addStatusListener((status) {
+              if (status == AnimationStatus.completed) {
+                Navigator.pushReplacement(context, ScaleRoute(page: Home()));
+              }
+            });
+            animationController
+              ..duration = composition.duration
+              ..forward();
+          },
+        ),
+      ),
+    );
   }
 }
