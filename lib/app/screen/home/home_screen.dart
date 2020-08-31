79d4c98eaa5a86629c0c04dd65f240f57c653a2c
renankanu <renankanu@gmail.com>
2020-08-30 21:53:39 -0300

    Feature - insert Cairo font

--- lib/app/screen/home/home_screen.dart
+++ lib/app/screen/home/home_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Colors.amber,
+        child: Center(
+          child: Text(
+            "Home Screen",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: "Cairo",
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
