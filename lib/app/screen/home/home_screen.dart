8f1a65edee4c2d111ea3336c63538c0c8063f31b
renankanu <renankanu@gmail.com>
2020-08-31 21:53:19 -0300

    Feature - home create

--- lib/app/screen/home/home_screen.dart
+++ lib/app/screen/home/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:app_meditation/app/styles/colors.dart';
 import 'package:app_meditation/app/utils/images.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -13,10 +15,134 @@ class Home extends StatelessWidget {
             Container(
               height: size.height * .45,
               decoration: BoxDecoration(
-                  color: Color(0xFFF5CEB8),
-                  image: DecorationImage(image: AssetImage(Images.logoPath))),
-            )
+                color: Color(0xFFF5CEB8),
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  image: AssetImage(Images.pilates_gpdb),
+                ),
+              ),
+            ),
+            SafeArea(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 52,
+                      height: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset(Images.menu),
+                    ),
+                  ),
+                  Text(
+                    "Good Morning \nPeople",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 30.0),
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Search",
+                          icon: SvgPicture.asset(Images.search),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: [
+                        CardCategory(
+                          svgPath: Images.hamburger,
+                          title: "Diet recommendation",
+                        ),
+                        CardCategory(
+                          svgPath: Images.excercise,
+                          title: "Kegel Exercises",
+                        ),
+                        CardCategory(
+                          svgPath: Images.meditation,
+                          title: "Meditation",
+                        ),
+                        CardCategory(
+                          svgPath: Images.yoga,
+                          title: "Yoga",
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ))
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardCategory extends StatelessWidget {
+  final String svgPath;
+  final String title;
+  const CardCategory({
+    Key key,
+    this.svgPath,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(13.0),
+        boxShadow: [
+          BoxShadow(
+              offset: Offset(0, 17),
+              blurRadius: 17,
+              spreadRadius: -23,
+              color: kShadowColor)
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Spacer(),
+                SvgPicture.asset(svgPath),
+                Spacer(),
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      .copyWith(fontSize: 15),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/app/utils/images.dart
+++ lib/app/utils/images.dart
@@ -1,3 +1,9 @@
 class Images {
-  static const String logoPath = 'assets/images/undraw_pilates_gpdb.png';
+  static const String pilates_gpdb = 'assets/images/undraw_pilates_gpdb.png';
+  static const String menu = 'assets/icons/menu.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String hamburger = 'assets/icons/Hamburger.svg';
+  static const String excercise = 'assets/icons/Excrecises.svg';
+  static const String meditation = 'assets/icons/Meditation.svg';
+  static const String yoga = 'assets/icons/yoga.svg';
 }
